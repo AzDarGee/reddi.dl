c56411c91c9efb031104d4c3c14ed0b6e4efd364
Ashish Darji <ashishdarji88@gmail.com>
2019-12-13 02:08:05 -0500

    styling, api

--- main.js
+++ main.js
@@ -17,18 +17,22 @@ $( document ).ready(function() {
 	        	document.getElementById(oldSubReddit).remove();	
 	        }
 
-	        $.get( "https://www.reddit.com/r/" + subreddit + "/hot.json?limit=1000", function( reddit ) {
-
-			i = 0;
-			for (i = 0; i < reddit['data']['children'].length; i++) {
-		        var img = document.createElement('img');
-				img.setAttribute('class', 'reddit-img');
-				img.setAttribute('onerror', 'this.style.display="none"');
-				img.src = reddit['data']['children'][i]['data']['url'];
-				div.appendChild(img);
-			}
-			oldSubReddit = subreddit;
-	    });
+	        $.get( "https://www.reddit.com/r/" + subreddit + "/hot.json?limit=100", function( reddit ) {
+	        	console.log(reddit);
+				i = 0;
+				for (i = 0; i < reddit['data']['children'].length; i++) {
+					var div2 = document.createElement('div');
+		        	div2.setAttribute('id', "subredditInnerDiv");
+			        var img = document.createElement('img');
+					img.setAttribute('class', 'reddit-img');
+					img.setAttribute('onerror', 'this.style.display="none"');
+					img.setAttribute('alt', reddit['data']['children'][i]['data']['title']);
+					img.src = reddit['data']['children'][i]['data']['url'];
+					div2.appendChild(img);
+					div.appendChild(div2);
+				}
+				oldSubReddit = subreddit;
+		    });
 	    }
 	});
 
